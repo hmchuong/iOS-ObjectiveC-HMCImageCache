8a57acd84843c481628a53944f602de43db3e0d3
hmchuong <minhchuong.itus@gmail.com>
2017-11-04 20:39:15 +0700

    Fix setImageFromURL with small image for button and imageView

--- HMCImageCache/Classes/UIButton+HMCImageCacheExtension.m
+++ HMCImageCache/Classes/UIButton+HMCImageCacheExtension.m
@@ -16,6 +16,17 @@
     if (self.frame.size.height == 0 && self.frame.size.width == 0) {
         return;
     }
+    // Choosing target size
+    CGImageSourceRef source = CGImageSourceCreateWithURL((CFURLRef)url, NULL);
+    NSDictionary* imageHeader = (__bridge NSDictionary*) CGImageSourceCopyPropertiesAtIndex(source, 0, NULL);
+    CGFloat height = [[imageHeader objectForKey:@"PixelHeight"] floatValue];
+    CGFloat width = [[imageHeader objectForKey:@"PixelWidth"] floatValue];
+    CGSize targetSize = self.frame.size;
+    
+    if (targetSize.height * targetSize.width > height * width) {
+        targetSize.height = height;
+        targetSize.width = width;
+    }
     [HMCImageCache.sharedInstance imageFromURL:url
                                 withTargetSize:self.frame.size
                                     completion:^(UIImage *image) {
