8684f2b47da943f357194df38b36303ba55b0c63
hmchuong <minhchuong.itus@gmail.com>
2017-11-04 11:37:30 +0700

    Button extension for load image from url

---
+++ HMCImageCache/Classes/UIButton+HMCImageCacheExtension.h
@@ -0,0 +1,12 @@
+//
+//  UIButton+HMCImageCacheExtension.h
+//  GoogleToolboxForMac
+//
+//  Created by Huỳnh Minh Chương on 11/4/17.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIButton_HMCImageCacheExtension : NSObject
+
+@end
---
+++ HMCImageCache/Classes/UIButton+HMCImageCacheExtension.m
@@ -0,0 +1,12 @@
+//
+//  UIButton+HMCImageCacheExtension.m
+//  GoogleToolboxForMac
+//
+//  Created by Huỳnh Minh Chương on 11/4/17.
+//
+
+#import "UIButton+HMCImageCacheExtension.h"
+
+@implementation UIButton_HMCImageCacheExtension
+
+@end
